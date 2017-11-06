6f41197423655f33a5e51e801322737e96c89546
Saeed Mirzamohammadi <smirzamo@uci.edu>
2017-11-06 10:00:02 -0800

    license

--- companion.c
+++ companion.c
@@ -1,3 +1,26 @@
+/*
+ * Ditio Companion module
+ * File: companion.c
+ *
+ * Copyright (c) 2017 University of California, Irvine, CA, USA
+ * All rights reserved.
+ *
+ * Authors: Saeed Mirzamohammadi <saeed@uci.edu>
+ * Ardalan Amiri Sani   <arrdalan@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,7 +28,7 @@
 #include <stdint.h>
 #include <time.h>
 //#include "include/aes.h"
-#define printk1 printf
+#define printk1 //printf
 
 int st_c = 0;
 struct start_stop_t
